c63695c7a0329449b8875c1fb4e4cf14dfe3b3bf
Scott Thiessen <scott@movingobjects.io>
2022-06-13 13:17:06 -0700

    Create base setup

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from './index.module.scss';
+
+export default function App() {
+
+  return (
+    <div className={style.wrap}>
+      <h3>Hello</h3>
+    </div>
+  )
+
+}
